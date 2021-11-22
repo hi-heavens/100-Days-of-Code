40cfad8c5a9d5541207f71d61bceafa9f6c11bd5
Kehinde Adedokun <ken_adex@yahoo.com>
2021-11-22 02:03:08 +0100

    A function that checks whether if the number passed into it is a prime number or not

---
+++ day-8/2-prime-number.py
@@ -0,0 +1,21 @@
+#Write your code below this line 👇
+def prime_checker(number):
+    is_prime = True
+    if number < 1:
+        print("Try again!")
+    else:
+        for num in range(2, number):
+            if number % num == 0:
+                is_prime = False
+
+        if not is_prime:
+            print("It's not a prime number.")
+        else:
+            print("It's a prime number.")
+
+
+#Write your code above this line 👆
+    
+#Do NOT change any of the code below👇
+n = int(input("Check this number: "))
+prime_checker(number=n)
